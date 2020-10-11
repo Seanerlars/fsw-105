7703e57964a2683bb00dd2b3a370a50aeb904b52
Seanerlars <Sean.larsen@bryanuniversity.edu>
2020-10-11 16:09:05 -0500

    functions

---
+++ week3/functions/functions.js
@@ -0,0 +1,28 @@
+function sum(num1, num2){
+    console.log(num1 + num2)
+}
+sum(20,17)
+
+function large(data){
+    console.log(Math.max (10, 5, 19))
+}
+large([10,5,19])
+
+
+    
+
+function isOdd(n) {
+    return n % 2 == 1;
+ }
+ var result = isOdd(11)
+ console.log(result + " it is odd")
+
+
+
+ function string(s){
+     for( var i = 0; i <= 20; i++)
+     return ("my name is Sean Larsen")}
+
+var result = string("My name is Sean Larsen and I am trying very hard ")
+console.log(result)
+ string("My name is Sean Larsen and I am trying very hard ")
